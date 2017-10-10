8c2ba6b9bb5571cfdd92d1c7915c5f2516e56bb4
OlBesp <olbesp365@gmail.com>
2017-10-10 15:38:50 +0300

    Create 'Sounds' object

--- js/script.js
+++ js/script.js
@@ -3,9 +3,120 @@ $(document).ready(function(){
 	// Sounds
 
 	var sounds = {
-		hat1: {
-			rock: new Howl({
-				src: ["sounds/a/hat1.wav"]
+		rock: {
+			w: new Howl({
+				src: ["sounds/rock/hat1.mp3"]
+			}),
+			a: new Howl({
+				src: ["sounds/rock/hat2.mp3"]
+			}),
+			s: new Howl({
+				src: ["sounds/rock/hat3.mp3"]
+			}),
+			d: new Howl({
+				src: ["sounds/rock/hat4.mp3"]
+			}),
+			i: new Howl({
+				src: ["sounds/rock/stick.mp3"]
+			}),
+			j: new Howl({
+				src: ["sounds/rock/snare1.mp3"]
+			}),
+			k: new Howl({
+				src: ["sounds/rock/snare2.mp3"]
+			}),
+			l: new Howl({
+				src: ["sounds/rock/snare3.mp3"]
+			}),
+			space: new Howl({
+				src: ["sounds/rock/kick.mp3"]
+			})
+		},
+		hipHop: {
+			w: new Howl({
+				src: ["sounds/hipHop/hat1.mp3"]
+			}),
+			a: new Howl({
+				src: ["sounds/hipHop/hat2.mp3"]
+			}),
+			s: new Howl({
+				src: ["sounds/hipHop/hat3.mp3"]
+			}),
+			d: new Howl({
+				src: ["sounds/hipHop/hat4.mp3"]
+			}),
+			i: new Howl({
+				src: ["sounds/hipHop/stick.mp3"]
+			}),
+			j: new Howl({
+				src: ["sounds/hipHop/snare1.mp3"]
+			}),
+			k: new Howl({
+				src: ["sounds/hipHop/snare2.mp3"]
+			}),
+			l: new Howl({
+				src: ["sounds/hipHop/snare3.mp3"]
+			}),
+			space: new Howl({
+				src: ["sounds/hipHop/kick.mp3"]
+			})
+		},
+		dance: {
+			w: new Howl({
+				src: ["sounds/dance/hat1.mp3"]
+			}),
+			a: new Howl({
+				src: ["sounds/dance/hat2.mp3"]
+			}),
+			s: new Howl({
+				src: ["sounds/dance/hat3.mp3"]
+			}),
+			d: new Howl({
+				src: ["sounds/dance/hat4.mp3"]
+			}),
+			i: new Howl({
+				src: ["sounds/dance/stick.mp3"]
+			}),
+			j: new Howl({
+				src: ["sounds/dance/snare1.mp3"]
+			}),
+			k: new Howl({
+				src: ["sounds/dance/snare2.mp3"]
+			}),
+			l: new Howl({
+				src: ["sounds/dance/snare3.mp3"]
+			}),
+			space: new Howl({
+				src: ["sounds/dance/kick.mp3"]
+			})
+		},
+		dubstep: {
+			w: new Howl({
+				src: ["sounds/dub/hat1.mp3"]
+			}),
+			a: new Howl({
+				src: ["sounds/dub/hat2.mp3"]
+			}),
+			s: new Howl({
+				src: ["sounds/dub/hat3.mp3"]
+			}),
+			d: new Howl({
+				src: ["sounds/dub/hat4.mp3"]
+			}),
+			i: new Howl({
+				src: ["sounds/dub/stick.mp3"]
+			}),
+			j: new Howl({
+				src: ["sounds/dub/snare1.mp3"]
+			}),
+			k: new Howl({
+				src: ["sounds/dub/snare2.mp3"]
+			}),
+			l: new Howl({
+				src: ["sounds/dub/snare3.mp3"]
+			}),
+			space: new Howl({
+				src: ["sounds/dub/kick.mp3"]
 			})
 		}
 	};
@@ -14,35 +125,35 @@ $(document).ready(function(){
 	$("body").keydown(function(event) {
 		if (event.which === 87) {
 			$(".hat1").addClass("pressed");
-			sounds.hat1.rock.play();
+			sounds.dance.w.play();
 		}
 	});
 	// var hat1 = new Howl({
-	// 	src: ["sounds/a/hat1.wav"]
+	// 	src: ["sounds/a/hat1.mp3"]
 	// });
 	// var hat2 = new Howl({
-	// 	src: ["sounds/a/hat2.wav"]
+	// 	src: ["sounds/a/hat2.mp3"]
 	// });
 	// var hat3 = new Howl({
-	// 	src: ["sounds/a/hat3.wav"]
+	// 	src: ["sounds/a/hat3.mp3"]
 	// });
 	// var hat4 = new Howl({
-	// 	src: ["sounds/a/hat4.wav"]
+	// 	src: ["sounds/a/hat4.mp3"]
 	// });
 	// var stick = new Howl({
-	// 	src: ["sounds/a/stick.wav"]
+	// 	src: ["sounds/a/stick.mp3"]
 	// });
 	// var snare1 = new Howl({
-	// 	src: ["sounds/a/snare1.wav"]
+	// 	src: ["sounds/a/snare1.mp3"]
 	// });
 	// var snare2 = new Howl({
-	// 	src: ["sounds/a/snare2.wav"]
+	// 	src: ["sounds/a/snare2.mp3"]
 	// });
 	// var snare3 = new Howl({
-	// 	src: ["sounds/a/snare3.wav"]
+	// 	src: ["sounds/a/snare3.mp3"]
 	// });
 	// var kick = new Howl({
-	// 	src: ["sounds/a/kick.wav"]
+	// 	src: ["sounds/a/kick.mp3"]
 	// });
 	//
 	// var keyCodes = [87, 65, 83, 68, 73, 74, 75, 76, 32];
