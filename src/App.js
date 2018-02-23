dd66b2cccbf655a04d770f2ee9e165de726a3aee
olbesp <olbesp365@gmail.com>
2018-02-23 17:00:27 +0200

    Fix toolbar and menu layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
+import Menu from './components/Toolbar/Menu/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Toolbar />
+        <Menu />
       </div>
     );
   }
--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,9 @@ import React from 'react';
 import styles from './Backdrop.css';
 
 const backdrop = (props) => (
-  props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
+  <div className={styles.Backdrop} onClick={props.clicked}></div> 
 );
 
 export default backdrop;
+
+// props.show ? : null
