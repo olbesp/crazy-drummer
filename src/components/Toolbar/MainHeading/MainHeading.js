5b6bbb7c85fdc87ab022270b1e06d1fb2c8d752d
olbesp <olbesp365@gmail.com>
2018-02-23 16:19:19 +0200

    Create MainHeading component

---
+++ src/components/Toolbar/MainHeading/MainHeading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './MainHeading.css';
+
+const mainHeading = () => (
+  <h1 className={styles.MainHeading}>Crazy Drummer</h1>
+);
+
+export default mainHeading;
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import styles from './Toolbar.css';
-// import MainHeading from './MainHeading/MainHeading';
+import MainHeading from './MainHeading/MainHeading';
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
-    <div>Heading</div>
+    <MainHeading />
     <div>Toggler</div>
     <nav>
       Navigation
