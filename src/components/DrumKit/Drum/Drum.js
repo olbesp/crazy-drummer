cd731668583921839fc6f097d9590a116c3338cd
olbesp <olbesp365@gmail.com>
2018-02-24 15:11:16 +0200

    Remove react-howler

--- src/components/DrumKit/Drum/Drum.js
+++ src/components/DrumKit/Drum/Drum.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import styles from './Drum.css';
+import Aux from '../../../hoc/Aux';
+import ReactHowler from 'react-howler';
 
 const drum = (props) => {
   
   return (
-    <div className={[styles.Drum, styles[props.drumClass]].join(' ')}>{props.drumButton}</div>
+    <Aux>
+      <ReactHowler
+        src={props.sound}
+        playing={props.play}
+      />
+      <div className={[styles.Drum, styles[props.drumClass]].join(' ')}>{props.drumButton}</div>
+    </Aux>
   );
 }
 
--- src/components/DrumKit/DrumKit.js
+++ src/components/DrumKit/DrumKit.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './DrumKit.css';
 import Background from '../../assets/images/floor.jpeg';
 import Drum from './Drum/Drum';
 
-const drumKit = (props) => {
-  const drums = [
-    {drumClass: 'Hat1', drumButton: 'i'},
-    {drumClass: 'Hat2', drumButton: 'j'},
-    {drumClass: 'Hat3', drumButton: 'k'},
-    {drumClass: 'Hat4', drumButton: 'l'},
-    {drumClass: 'Snare1', drumButton: 'w'},
-    {drumClass: 'Snare2', drumButton: 'a'},
-    {drumClass: 'Snare3', drumButton: 's'},
-    {drumClass: 'Snare4', drumButton: 'd'},
-    {drumClass: 'Kick', drumButton: 'space'}
-  ].map((drum, index) => {
-    return <Drum key={index} drumClass={drum.drumClass} drumButton={drum.drumButton} />;
-  });
-  return (
-    <div className={styles.DrumKit} style={{ 
-      backgroundImage: `radial-gradient(rgba(0, 0, 0, 0), rgba(18, 12, 45, .8)), url(${Background})` 
-    }}>{drums}</div>
-  );
+class DrumKit extends Component {
+  render() {
+    const drums = [
+      { drumClass: 'Hat1', drumButton: 'i' },
+      { drumClass: 'Hat2', drumButton: 'j' },
+      { drumClass: 'Hat3', drumButton: 'k' },
+      { drumClass: 'Hat4', drumButton: 'l' },
+      { drumClass: 'Snare1', drumButton: 'w' },
+      { drumClass: 'Snare2', drumButton: 'a' },
+      { drumClass: 'Snare3', drumButton: 's' },
+      { drumClass: 'Snare4', drumButton: 'd' },
+      { drumClass: 'Kick', drumButton: 'space' }
+    ].map((drum, index) => {
+      return <Drum key={index} drumClass={drum.drumClass} drumButton={drum.drumButton} />;
+    });
+
+    return (
+      <div className={styles.DrumKit} style={{
+        backgroundImage: `radial-gradient(rgba(0, 0, 0, 0), rgba(18, 12, 45, .8)), url(${Background})`
+      }}>{drums}</div>
+    );
+  }
 }
 
-export default drumKit;
+
+export default DrumKit;
