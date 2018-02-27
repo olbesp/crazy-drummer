654d69c3692def253c2d82b0c613c300a4d92202
olbesp <olbesp365@gmail.com>
2018-02-27 12:55:33 +0200

    Restructure project folder

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Toolbar from '../components/Toolbar/Toolbar';
+import Menu from '../components/Toolbar/Menu/Menu';
+import DrumKit from './DrumKit/DrumKit';
+
+class App extends Component {
+  state = {
+    showMenu: false,
+    activeGenre: 'rock'
+  }
+
+  menuClosedHandler = () => {
+    this.setState({ showMenu: false });
+  }
+
+  menuToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showMenu: !prevState.showMenu };
+    });
+  }
+
+  genreChangedHandler = (e) => {
+    this.setState({
+      activeGenre: e.target.textContent.toLowerCase()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Toolbar 
+          togglerClicked={this.menuToggleHandler}
+          open={this.state.showMenu}
+          closed={this.menuClosedHandler}
+        />
+        <Menu 
+          open={this.state.showMenu}
+          closed={this.menuClosedHandler}
+          changeGenre={this.genreChangedHandler}
+          genre={this.state.activeGenre}
+        />
+        <DrumKit genre={this.state.activeGenre} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/DrumKit/DrumKit.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+
+import styles from './DrumKit.css';
+import Background from '../../assets/images/floor.jpeg';
+import Drum from '../../components/Drum/Drum';
+import library from '../../components/Library/Library';
+
+const drums = [
+  { drumClass: 'Hat1', drumButton: 'i' },
+  { drumClass: 'Hat2', drumButton: 'j' },
+  { drumClass: 'Hat3', drumButton: 'k' },
+  { drumClass: 'Hat4', drumButton: 'l' },
+  { drumClass: 'Snare1', drumButton: 'w' },
+  { drumClass: 'Snare2', drumButton: 'a' },
+  { drumClass: 'Snare3', drumButton: 's' },
+  { drumClass: 'Snare4', drumButton: 'd' },
+  { drumClass: 'Kick', drumButton: 'space' }
+];
+
+class DrumKit extends Component {
+  state = {
+    drumKeyCode: null
+  }
+  
+  drumPressedHandler = (e) => {
+    if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode === 32) {
+      this.setState({ drumKeyCode: e.keyCode });
+    }
+  }
+
+  drumPressedReleaseHandler = () => {
+    if (this.state.drumKeyCode) {
+      this.setState({ drumKeyCode: null });
+    }
+  }
+
+  drumClickedDownHandler = (e) => {
+    if (e.target.id) {
+      let keyCode;
+      switch (e.target.id.toLowerCase()) {
+        case 'i': keyCode = 73; break;
+        case 'j': keyCode = 74; break;
+        case 'k': keyCode = 75; break;
+        case 'l': keyCode = 76; break;
+        case 'w': keyCode = 87; break;
+        case 'a': keyCode = 65; break;
+        case 's': keyCode = 83; break;
+        case 'd': keyCode = 68; break;
+        case 'space': keyCode = 32; break;
+        default: keyCode = null;
+      }
+      this.setState({ drumKeyCode: keyCode });
+    }
+  }
+
+  drumClickedUpHandler = (e) => {
+    if (this.state.drumKeyCode) {
+      this.setState({ drumKeyCode: null });
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.drumPressedHandler);
+    document.addEventListener('keyup', this.drumPressedReleaseHandler);
+  }
+
+  componentDidUpdate() {
+    if (((this.state.drumKeyCode >= 65 && this.state.drumKeyCode <= 90) 
+      || this.state.drumKeyCode === 32) && this.props.genre) {
+      const drum = library[this.props.genre][`${this.state.drumKeyCode}`]
+      if (drum) {
+        drum.sound.play();
+      } else {
+        library.effects[Math.floor(Math.random() * library.effects.length)].play();
+      }
+    }
+  }
+
+  render() {
+    const DOMdrums = drums.map((drum, index) => {
+      return <Drum 
+          key={index} 
+          drumClass={drum.drumClass} 
+          drumButton={drum.drumButton}
+        />;
+    });
+
+    return (
+      <div
+        className={styles.DrumKit} 
+        style={{
+          backgroundImage: `radial-gradient(rgba(0, 0, 0, 0), rgba(18, 12, 45, .8)), url(${Background})`
+        }}
+        onMouseDown={this.drumClickedDownHandler}
+        onMouseUp={this.drumClickedUpHandler}
+      >
+        <div className={styles.DrumsContainer}>
+          {DOMdrums}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default DrumKit;
