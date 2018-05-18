d14caf7dc896f288de48ece1af6f736dbd8586d7
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-18 09:44:16 +0300

    Redux-thunk setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import drumsReducer from './store/reducers/drums';
 import genresReducer from './store/reducers/genres';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const rootReducer = combineReducers({
   drums: drumsReducer,
   genres: genresReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
